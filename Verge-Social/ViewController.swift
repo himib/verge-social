6fa75e9fe2e41cc38a05d72c5f4175583294ce33
Gopal Bhojwani <gopal@gopals-mbp.net.sahm.ae>
2017-07-06 19:25:45 +0400

    Initial Commit

---
+++ Verge-Social/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Verge-Social
+//
+//  Created by Gopal Bhojwani on 7/6/17.
+//  Copyright © 2017 Hemanshu Bhojwani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
